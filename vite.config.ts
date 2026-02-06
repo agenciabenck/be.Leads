29a2b6672066043c4a577171a672a236e5cf3df2
BeLeads Dev <dev@beleads.com>
2026-02-06 15:55:55 -0300

    fix: total reset to index.js and entry.html for hostinger compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'vite-dev-entry.html'),
+        main: path.resolve(__dirname, 'entry.html'),
       },
       output: {
         entryFileNames: `assets/[name]-[hash].js`,
